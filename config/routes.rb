6d88021b183dc237fd7d17d37da20b3f335f33eb
Ricardo <ei09045@fe.up.pt>
2012-11-02 18:02:26 +0000

    6

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ UserAuth::Application.routes.draw do
 
   match ':controller(/:action(/:id))(.:format)'
   root :to => 'sessions#login'
-  match "signup", :to => "users#new"
-  match "login", :to => "sessions#login"
-  match "logout", :to => "sessions#logout"
-  match "home", :to => "sessions#home"
-  match "profile", :to => "sessions#profile"
-  match "setting", :to => "sessions#setting"
+  match "signup", :to => 'users#new'
+  match "login", :to => 'sessions#login'
+  match "logout", :to => 'sessions#logout'
+  match "home", :to => 'sessions#home'
+  match "profile", :to => 'sessions#profile'
+  match "setting", :to => 'sessions#setting'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
