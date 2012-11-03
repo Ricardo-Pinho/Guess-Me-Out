bfbf6f619e1f40c720e0912c7743b923963fec12
Ricardo <ei09045@fe.up.pt>
2012-11-03 19:19:49 +0000

    last_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   protected
   def authenticate_user
-  	unless session[:user_id]
+  	/*unless session[:user_id]
   		redirect_to(:controller => 'sessions', :action => 'login')
   		return false
   	else
       # set current_user by the current user object
       @current_user = User.find session[:user_id] 
   		return true
-  	end
+  	end*/
+	redirect_to(:controller => 'sessions', :action => 'login')
   end
 
   #This method for prevent user to access Signup & Login Page without logout
