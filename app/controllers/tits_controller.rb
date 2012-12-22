4354865b1649c79c116274debe50aa4d3c0583ad
Joao Lopes <joaopclopes@gmail.com>
2012-12-22 18:59:36 +0000

    tits

---
+++ app/controllers/tits_controller.rb
@@ -0,0 +1,8 @@
+class TitsController < ApplicationController
+
+	def index
+		respond_to do |format|
+    		format.html
+    	end
+	end
+end
---
+++ test/unit/helpers/tits_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TitsHelperTest < ActionView::TestCase
+end
