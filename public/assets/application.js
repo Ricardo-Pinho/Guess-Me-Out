28d6258863360abbfe592bd31c6ac1acc3b0e9fd
Ricardo <ei09045@fe.up.pt>
2012-12-30 21:55:59 +0000

    heroku test once more

--- public/assets/application.js
+++ public/assets/application.js
@@ -10106,6 +10106,9 @@ $(document).ready(function() {
 
             if ($(".editsvg").length>0)
             {
+                $.ajaxSetup({
+                    async: false
+                });
                 var user=parseInt($("svg:eq("+0+")").attr('data-user'));
                 $.getJSON(path+"svgs_user_android",{userid:user,componentid:1},function(data){addType(data,hairType,"hairType");});
                 $.getJSON(path+"svgs_user_android",{userid:user,componentid:2},function(data){addColor(data,hairColor,"hairColor");});
@@ -10179,6 +10182,9 @@ $(document).ready(function() {
             }
             else
             {
+                $.ajaxSetup({
+                    async: false
+                });
                 $.getJSON(path+"svgs_android",{componentid:1},function(data){addType(data,hairType,"");});
                 $.getJSON(path+"svgs_android",{componentid:2},function(data){addColor(data,hairColor,"");});
                 $.getJSON(path+"svgs_android",{componentid:3},function(data){addColor(data,skinColor,"");});
