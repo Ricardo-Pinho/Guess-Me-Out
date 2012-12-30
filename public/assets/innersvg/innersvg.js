228f73ab59c7555ab4701c4f62b30c1bc062b38c
Ricardo <ei09045@fe.up.pt>
2012-12-30 08:49:28 +0000

    update for heroku

--- public/assets/innersvg/innersvg.js
+++ public/assets/innersvg/innersvg.js
@@ -10,4 +10,89 @@
  *
  * I haven't decided on the best name for this property - thus the duplication.
  */
-(function(){var e=function(t,n){var r=t.nodeType;if(r==3)n.push(t.textContent.replace(/&/,"&amp;").replace(/</,"&lt;").replace(">","&gt;"));else if(r==1){n.push("<",t.tagName);if(t.hasAttributes()){var i=t.attributes;for(var s=0,o=i.length;s<o;++s){var u=i.item(s);n.push(" ",u.name,"='",u.value,"'")}}if(t.hasChildNodes()){n.push(">");var a=t.childNodes;for(var s=0,o=a.length;s<o;++s)e(a.item(s),n);n.push("</",t.tagName,">")}else n.push("/>")}else{if(r!=8)throw"Error serializing XML. Unhandled node of type: "+r;n.push("<!--",t.nodeValue,"-->")}};Object.defineProperty(SVGElement.prototype,"innerHTML",{get:function(){var t=[],n=this.firstChild;while(n)e(n,t),n=n.nextSibling;return t.join("")},set:function(e){while(this.firstChild)this.removeChild(this.firstChild);try{var t=new DOMParser;t.async=!1,sXML="<svg xmlns='http://www.w3.org/2000/svg'>"+e+"</svg>";var n=t.parseFromString(sXML,"text/xml").documentElement,r=n.firstChild;while(r)this.appendChild(this.ownerDocument.importNode(r,!0)),r=r.nextSibling}catch(i){throw new Error("Error parsing XML string")}}}),Object.defineProperty(SVGElement.prototype,"innerSVG",{get:function(){return this.innerHTML},set:function(e){this.innerHTML=e}})})();
+
+
+(function() {
+var serializeXML = function(node, output) {
+  var nodeType = node.nodeType;
+  if (nodeType == 3) { // TEXT nodes.
+    // Replace special XML characters with their entities.
+    output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
+  } else if (nodeType == 1) { // ELEMENT nodes.
+    // Serialize Element nodes.
+    output.push('<', node.tagName);
+    if (node.hasAttributes()) {
+      var attrMap = node.attributes;
+      for (var i = 0, len = attrMap.length; i < len; ++i) {
+        var attrNode = attrMap.item(i);
+        output.push(' ', attrNode.name, '=\'', attrNode.value, '\'');
+      }
+    }
+    if (node.hasChildNodes()) {
+      output.push('>');
+      var childNodes = node.childNodes;
+      for (var i = 0, len = childNodes.length; i < len; ++i) {
+        serializeXML(childNodes.item(i), output);
+      }
+      output.push('</', node.tagName, '>');
+    } else {
+      output.push('/>');
+    }
+  } else if (nodeType == 8) {
+    // TODO(codedread): Replace special characters with XML entities?
+    output.push('<!--', node.nodeValue, '-->');
+  } else {
+    // TODO: Handle CDATA nodes.
+    // TODO: Handle ENTITY nodes.
+    // TODO: Handle DOCUMENT nodes.
+    throw 'Error serializing XML. Unhandled node of type: ' + nodeType;
+  }
+}
+// The innerHTML DOM property for SVGElement.
+Object.defineProperty(SVGElement.prototype, 'innerHTML', {
+  get: function() {
+    var output = [];
+    var childNode = this.firstChild;
+    while (childNode) {
+      serializeXML(childNode, output);
+      childNode = childNode.nextSibling;
+    }
+    return output.join('');
+  },
+  set: function(markupText) {
+    // Wipe out the current contents of the element.
+    while (this.firstChild) {
+      this.removeChild(this.firstChild);
+    }
+
+    try {
+      // Parse the markup into valid nodes.
+      var dXML = new DOMParser();
+      dXML.async = false;
+      // Wrap the markup into a SVG node to ensure parsing works.
+      sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\'>' + markupText + '</svg>';
+      var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
+
+      // Now take each node, import it and append to this element.
+      var childNode = svgDocElement.firstChild;
+      while(childNode) {
+        this.appendChild(this.ownerDocument.importNode(childNode, true));
+        childNode = childNode.nextSibling;
+      }
+    } catch(e) {
+      throw new Error('Error parsing XML string');
+    };
+  }
+});
+
+// The innerSVG DOM property for SVGElement.
+Object.defineProperty(SVGElement.prototype, 'innerSVG', {
+  get: function() {
+    return this.innerHTML;
+  },
+  set: function(markupText) {
+    this.innerHTML = markupText;
+  }
+});
+
+})();
